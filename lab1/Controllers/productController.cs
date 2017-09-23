12dea5f9ad7f862fda171404960e9ddd00c71c4f
raman23011997 <sramanjeet57@gmail.com>
2017-09-22 21:47:48 -0400

    Lab1
    Lab1

---
+++ lab1/Controllers/productController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class productController : Controller
+    {
+        // GET: product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult details(String product)
+        {
+            ViewBag.Message= "You selected product:"+product;
+
+            return View();
+        }
+    }
+}
